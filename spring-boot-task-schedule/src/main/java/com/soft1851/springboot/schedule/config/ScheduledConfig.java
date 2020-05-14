542790890197de9d0ab1c32edc60e806513e29cd
czzeng-code <czzeng_ok@qq.com>
2020-05-14 23:02:07 +0800

    任务调度 起步

---
+++ spring-boot-task-schedule/src/main/java/com/soft1851/springboot/schedule/config/ScheduledConfig.java
@@ -0,0 +1,29 @@
+package com.soft1851.springboot.schedule.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Zeng
+ * @ClassName ScheduledConfig
+ * @Description 定时任务配置
+ * @Date 2020/5/14
+ * @Version 1.0
+ **/
+@Configuration
+public class ScheduledConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        scheduledTaskRegistrar.setScheduler(setTaskExecutors());
+    }
+
+    @Bean
+    public Executor setTaskExecutors() {
+        return Executors.newScheduledThreadPool(3);
+    }
+}
